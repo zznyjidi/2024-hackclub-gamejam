bc5c1a7b39cfa30882f0b6e5d61bef655433e492
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-11-23 13:48:51 -0500

    Add 13-16 Tilemap

---
+++ scripts/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta):
+	
+	var direction_y = Input.get_axis("Player_Move_UP", "Player_Move_DOWN")
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	var direction_x = Input.get_axis("Player_Move_LEFT", "Player_Move_RIGHT")
+	if direction_x:
+		velocity.x = direction_x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()

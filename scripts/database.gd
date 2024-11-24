06975cb1dc18292227e72de4866a57bd78941661
Shinano <user@Shinano-2.local>
2024-11-23 23:29:52 -0500

    Edit Player Health and Damage

--- scripts/database.gd
+++ scripts/database.gd
@@ -1,10 +1,10 @@
 extends Node
 
 var playerPosition = Vector2(0, 0)
-var playerCurrentHP = 100
-var playerMaxHP = 100
+var playerCurrentHP = 50
+var playerMaxHP = 50
 var playerAttackSacler = 1.0
 
-var attackArrowDamage = 20
+var attackArrowDamage = 10
 
 var enemyVision = 500

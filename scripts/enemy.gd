3eb9c931d94eb855a7eb53b6601295df7e26c4f6
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-11-23 20:45:45 -0500

    [Archive]Add Bug Arrow

---
+++ scripts/enemy.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+const SPEED = 200.0
+
+func _physics_process(delta):
+#	print(Database.playerPosition - self.global_position)
+	var distance = hypot(Database.playerPosition - self.global_position)
+	if distance > 80:
+		var direction = (Database.playerPosition - self.global_position).normalized()
+		var acceleration = direction * SPEED/30
+		velocity += acceleration
+	elif distance > 60:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	else:
+		velocity = -(Database.playerPosition - self.global_position)
+
+	move_and_slide()
+
+func hypot(pos: Vector2) -> float:
+	return sqrt(pos.x**2 + pos.y**2)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,32 +1,34 @@
 extends CharacterBody2D
 
+var Arrow = preload("res://scenes/arrow.tscn")
 
 const SPEED = 300.0
 const RUN_SPEED = SPEED * 1.5
 const BLOCK_SIZE = 64
 
-
 func _physics_process(delta):
-	
-	var direction_y = Input.get_axis("Player_Move_UP", "Player_Move_DOWN")
-	if direction_y:
-		if Input.is_action_pressed("Player_Move_RUN"):
-			velocity.y = direction_y * RUN_SPEED
-		else:
-			velocity.y = direction_y * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
 	
 	var direction_x = Input.get_axis("Player_Move_LEFT", "Player_Move_RIGHT")
 	if direction_x:
-		if Input.is_action_pressed("Player_Move_RUN"):
-			velocity.x = direction_x * RUN_SPEED
-		else:
-			velocity.x = direction_x * SPEED
+		velocity.x = direction_x * (RUN_SPEED if Input.is_action_pressed("Player_Move_RUN") else SPEED)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	var direction_y = Input.get_axis("Player_Move_UP", "Player_Move_DOWN")
+	if direction_y:
+		velocity.y = direction_y * (RUN_SPEED if Input.is_action_pressed("Player_Move_RUN") else SPEED)
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	Database.playerPosition = self.global_position
 
 	move_and_slide()
 	#var collision = move_and_collide(velocity * delta)
 	#if collision:
 	#	velocity = velocity.slide(collision.get_normal())
+
+
+func _on_node_2d_player_attact():
+	var b = Arrow.instantiate()
+	b.start(self.global_position, 0)
+	get_tree().root.add_child(b)

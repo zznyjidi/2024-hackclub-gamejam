0815e7d565735927c95254bc2e93e396909081db
Shinano <user@Shinano-2.local>
2024-11-23 23:36:06 -0500

    Changed DataBase

--- scripts/database.gd
+++ scripts/database.gd
@@ -1,10 +1,10 @@
 extends Node
 
 var playerPosition = Vector2(0, 0)
-var playerCurrentHP = 50
-var playerMaxHP = 50
+var playerCurrentHP = 5
+var playerMaxHP = 5
 var playerAttackSacler = 1.0
 
-var attackArrowDamage = 10
+var attackArrowDamage = 1
 
 var enemyVision = 500
